bc994386f570a5158680a3d4ffc21b353ffe7ef1
yassine140903 <154000479+yassine140903@users.noreply.github.com>
2025-07-24 20:17:39 +0100

    hero

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LatestCollection = () => {
+    const {products} = useContext(ShopContext);
+    console.log(products);
+  
+  
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import ShopContextProvider from './context/ShopContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
+
+
